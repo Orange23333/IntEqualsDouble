0f496a24e61949c0b86b8706d567806b8dae2cac
Orange_233 <874462262@qq.com>
2020-03-28 04:54:13 +0800

    添加项目文件。

---
+++ IntEqualsDouble/EasyFile.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IntEqualsDouble
+{
+    public class EasyFile
+    {
+        private FileStream fs = null;
+
+        /// <summary>
+        /// 写入一个字节。
+        /// </summary>
+        /// <param name="bytes"></param>
+        public void WriteBytes(byte b)
+        {
+            fs.WriteByte(b);
+        }
+        /// <summary>
+        /// 写入字节数组。
+        /// </summary>
+        public void WriteBytes(byte[] bytes)
+        {
+            fs.Write(bytes, 0, bytes.Length);
+        }
+        /// <summary>
+        /// 写入一个EasyFile块。
+        /// </summary>
+        public void WriteBlock(byte[] bytes)
+        {
+            byte[] lengthBytes = BitConverter.GetBytes(bytes.Length);
+            WriteBytes(lengthBytes);
+
+            WriteBytes(bytes);
+        }
+        /// <summary>
+        /// 写入一个字符串。
+        /// </summary>
+        /// <param name="str">所写入的数据。</param>
+        public void WriteString(string str)
+        {
+            WriteBlock(Encoding.UTF8.GetBytes(str));
+        }
+        /// <summary>
+        /// 写入一个32位整数。
+        /// </summary>
+        /// <param name="i">所写入的数据。</param>
+        public void WriteInt32(int i)
+        {
+            WriteBlock(BitConverter.GetBytes(i));
+        }
+
+
+
+        /// <summary>
+        /// 读取一个字节。
+        /// </summary>
+        /// <returns></returns>
+        public byte ReadByte()
+        {
+            return (byte)fs.ReadByte();
+        }
+        /// <summary>
+        /// 读取指定数量字节。（不一定读了length字节。）
+        /// </summary>
+        /// <param name="length">预期读取字节。</param>
+        /// <returns>读取结果。</returns>
+        public byte[] ReadBytes(int length)
+        {
+            byte[] temp = new byte[length];
+            int fact = fs.Read(temp, 0, length);
+            return temp.Take(fact).ToArray();
+        }
+        /// <summary>
+        /// 读取一个EasyFile块。
+        /// </summary>
+        /// <returns>所读到的数据。</returns>
+        public byte[] ReadBlock()
+        {
+            byte[] lengthBytes = BitConverter.GetBytes((int)0);
+            fs.Read(lengthBytes, 0, lengthBytes.Length);
+
+            byte[] temp = new byte[BitConverter.ToInt32(lengthBytes, 0)];
+            fs.Read(temp, 0, temp.Length);
+            return temp;
+        }
+        /// <summary>
+        /// 读取一个字符串。
+        /// </summary>
+        /// <returns>所读到的数据。</returns>
+        public string ReadString()
+        {
+            return Encoding.UTF8.GetString(ReadBlock());
+        }
+        /// <summary>
+        /// 读取一个32位整数。
+        /// </summary>
+        /// <returns>所读到的数据。</returns>
+        public int ReadInt32()
+        {
+            return BitConverter.ToInt32(ReadBlock(), 0);
+        }
+
+        public EasyFile(ref FileStream fileStream)
+        {
+            if (fileStream == null)
+            {
+                throw new ArgumentNullException("The file stream can not be null.");
+            }
+            this.fs = fileStream;
+        }
+    }
+}
---
+++ IntEqualsDouble/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IntEqualsDouble
+{
+    class Program
+    {
+        static readonly string SaveTempPath = "save.tmp";
+
+        struct TaskInfo
+        {
+            public int begin;
+            public int span;
+        }
+        static void Main()
+        {
+            List<int> badList = new List<int>();//存储不符合检测预期的值得列表
+            int taskMax = 128;//其实可以根据当前运算速度，适时调整线程和线程任务量来优化速度，但是我懒
+            int spanPerTime = 1024;
+            Dictionary<Task<bool[]>, TaskInfo> tasks = new Dictionary<Task<bool[]>, TaskInfo>();
+            int totalFinishedTask = 0;
+
+            int InputPositiveInt32(string text)
+            {
+                bool flag = true;
+                int inputInt = 0;
+                string tempLine;
+                while (flag)
+                {
+                    Console.Write(text);
+                    tempLine = Console.ReadLine();
+                    try
+                    {
+                        inputInt = int.Parse(tempLine);
+                        if (inputInt > 0)
+                        {
+                            flag = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Not a positive number.");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Not a number.");
+                    }
+                }
+                return inputInt;
+            }
+            int ClearTasks(int timeout)
+            {
+                int cmpTaskIdx;
+                int cmpTaskCount = 0;
+
+                while (tasks.Count > 0)
+                {
+                    if (timeout >= 0)
+                    {
+                        cmpTaskIdx = Task<bool[]>.WaitAny(tasks.Keys.ToArray(), timeout);
+                        if (cmpTaskIdx == -1)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        cmpTaskIdx = Task<bool[]>.WaitAny(tasks.Keys.ToArray());
+                    }
+
+                    Task<bool[]> cmpTask = tasks.Keys.ToArray()[cmpTaskIdx];
+                    TaskInfo cmpTaskInfo = (from val in tasks
+                                            where val.Key == cmpTask
+                                            select val).Single().Value;
+                    bool[] cmpTaskResult = cmpTask.Result;
+
+                    CkResult(ref cmpTaskResult, cmpTaskInfo.begin, ref badList);
+                    totalFinishedTask++;
+                    cmpTaskCount++;
+
+                    tasks.Remove(cmpTask);
+                }
+                Console.WriteLine("Clear Cmp; badList.Count={0:D10}", badList.Count);
+                return cmpTaskCount;
+            }
+
+            //####从这开始###
+
+            int i = 0;
+            TaskInfo arg;
+            DateTime spdScan0 = DateTime.Now, spdScan1;
+
+            if ((new FileInfo(SaveTempPath)).Exists)
+            {
+                FileStream fs = new FileStream(SaveTempPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                EasyFile ef = new EasyFile(ref fs);
+                taskMax = ef.ReadInt32();
+                spanPerTime = ef.ReadInt32();
+                totalFinishedTask = ef.ReadInt32();
+                int badList_n = ef.ReadInt32();
+                for(int j = 0; j < badList_n; j++)
+                {
+                    badList.Add(ef.ReadInt32());
+                }
+                fs.Close();
+                i = (int)(int.MinValue + (long)totalFinishedTask * spanPerTime);
+            }
+            else
+            {
+                taskMax = InputPositiveInt32("taskMax = ");
+                spanPerTime = InputPositiveInt32("spanPerTime = ");
+                i = int.MinValue;
+            }
+
+            handler = new ConsoleEventDelegate(ConsoleEventCallback);
+            SetConsoleCtrlHandler(handler, true);
+
+            bool ConsoleEventCallback(int eventType)
+            {
+                //https://docs.microsoft.com/en-us/windows/console/handlerroutine?redirectedfrom=MSDN
+                const int CTRL_CLOSE_EVENT = 2;
+                if (eventType == CTRL_CLOSE_EVENT && needProtect)
+                {
+                    FileStream fs = new FileStream(SaveTempPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                    EasyFile ef = new EasyFile(ref fs);
+                    ef.WriteInt32(taskMax);
+                    ef.WriteInt32(spanPerTime);
+                    ef.WriteInt32(totalFinishedTask);
+                    ef.WriteInt32(badList.Count);
+                    for (int j = 0; j < badList.Count; j++)
+                    {
+                        ef.WriteInt32(badList[j]);
+                    }
+                    fs.Close();
+                }
+                return false;
+            }
+
+            while(true)
+            {
+                if ((long)int.MaxValue - i < spanPerTime)
+                {
+                    break;
+                }
+
+                if (tasks.Count >= taskMax)
+                {
+                    int cmpTaskCount = ClearTasks(1);
+                    spdScan1 = DateTime.Now;
+                    Console.WriteLine("#Now Speed: {0} task/s; {1} number/s",
+                        cmpTaskCount / spdScan1.Subtract(spdScan0).TotalMilliseconds * 1000,
+                        cmpTaskCount * spanPerTime / spdScan1.Subtract(spdScan0).TotalMilliseconds * 1000
+                        );
+                    spdScan0 = spdScan1;
+                }
+                arg = new TaskInfo
+                {
+                    begin = i,
+                    span = spanPerTime
+                };
+                tasks.Add(Task<bool[]>.Factory.StartNew(new Func<object, bool[]>(Cal), arg), arg);
+                needProtect = true;
+
+                i += spanPerTime;
+            }
+            //测算特殊范围
+            if (tasks.Count >= taskMax)
+            {
+                ClearTasks(1);
+            }
+            needProtect = false;//懒得改成计数完成到哪一步，所以这里关闭保护，避免出错
+            arg = new TaskInfo
+            {
+                begin = i,
+                span = int.MaxValue - i
+            };
+            tasks.Add(Task<bool[]>.Factory.StartNew(new Func<object, bool[]>(Cal), arg), arg);
+            ClearTasks(-1);
+            //单独计算int.MaxValue
+            arg = new TaskInfo
+            {
+                begin = int.MaxValue,
+                span = 1
+            };
+            if (!Cal(arg)[0])
+            {
+                badList.Add(int.MaxValue);
+            }
+
+            //输出结果
+            Console.WriteLine("badList[{0}]:", badList.Count);
+            foreach (int temp in badList)
+            {
+                Console.Write("{0}, ", temp);
+            }
+            Console.WriteLine("!!!This program work with .NET Core 3.1!!!");
+
+            Console.ReadLine();
+        }
+
+        private static bool needProtect = false;
+        //https://stackoverflow.com/questions/4646827/on-exit-for-a-console-application{
+        static ConsoleEventDelegate handler;   // Keeps it from getting garbage collected
+                                               // Pinvoke
+        private delegate bool ConsoleEventDelegate(int eventType);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+        //}
+
+        private static bool[] Cal(object argument)
+        {
+            DateTime beginDT = DateTime.Now;
+            TaskInfo taskInfo = (TaskInfo)argument;
+            bool[] rs = new bool[taskInfo.span];
+
+            double d;
+            int i, val;
+            for (i = 0; i < taskInfo.span; i++)
+            {
+                val = taskInfo.begin + i;
+                d = double.Parse(String.Format("{0}.0", val));
+                rs[i] = val == d;
+            }
+
+            //StringBuilder sb = new StringBuilder("");
+            int falseCount = 0;
+            foreach (bool r in rs)
+            {
+                //sb.Append(r ? "_" : "!");
+                if (!r)
+                {
+                    falseCount++;
+                }
+            }
+            DateTime endDT = DateTime.Now;
+            Console.WriteLine("[{0}~{1}]{2:X8}: {3:D10} ~ {4:D10} [{5:D10}]",
+                String.Format("{0}.{1:D3}", beginDT.ToString("HH:mm:ss"), beginDT.Millisecond),
+                String.Format("{0}.{1:D3}", endDT.ToString("HH:mm:ss"), endDT.Millisecond),
+                Thread.CurrentThread.ManagedThreadId,
+                taskInfo.begin,
+                taskInfo.begin + taskInfo.span - 1,
+                falseCount
+                );
+
+            return rs;
+        }
+
+        private static void CkResult(ref bool[] results, int beginIdx, ref List<int> badList)
+        {
+            int i;
+            for (i = 0; i < results.Length; i++)
+            {
+                if (!results[i])
+                {
+                    badList.Add(beginIdx + i);
+                }
+            }
+        }
+    }
+}
